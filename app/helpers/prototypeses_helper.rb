b686e1bed0a1957ab8f8477cfcba3acee1bf3450
tomoR2 <144089090+tomoR2@users.noreply.github.com>
2023-09-21 21:41:25 +0900

    first commit

---
+++ app/helpers/prototypeses_helper.rb
@@ -0,0 +1,2 @@
+module PrototypesesHelper
+end
---
+++ test/controllers/prototypeses_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PrototypesesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
